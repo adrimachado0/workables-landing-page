3f12ee0bb2eb53c11d81749924c04c8cccb5dd66
Adriel Machado <adri@newtonhewitt.com>
2023-06-22 12:55:06 -0300

    Sacando flecha

--- components/button/connectNow.jsx
+++ components/button/connectNow.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ConnectNow = ({info}) => {
   return (
     <div className='gradiente p-[1px] rounded-lg w-fit mx-auto'>
-        <p className='text-white px-6 py-1 rounded-lg'>{info} {'>'}</p>
+        <p className='text-white px-6 py-1 rounded-lg'>{info}</p>
     </div>
   )
 }
