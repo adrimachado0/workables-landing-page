c171c8d7b3e13c99248673bccf2cfe4330e9fe8c
Adriel Machado <adri@newtonhewitt.com>
2023-06-27 09:37:22 -0300

    Background WEBP

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ const questrial = Questrial({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${questrial.className}`}>
+      <body className={`${questrial.className} bg-[url('../../public/Backgrounds.webp')]`}>
         <Header />
         {children}
         <Footer />
