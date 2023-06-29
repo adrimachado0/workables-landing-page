e4c490855639cc10d4588e756ea94fc2c5523216
Adriel Machado <adri@newtonhewitt.com>
2023-06-29 09:59:50 -0300

    Arreglando imagenes y sparkwith

--- components/home/HomeSpark/homespark.jsx
+++ components/home/HomeSpark/homespark.jsx
@@ -4,11 +4,11 @@ import HomeSparkMain from './homesparkmain';
 const HomeSpark = () => {
   return (
     <div className='flex flex-col items-center gap-5'>
-        <h2 className='home-spark-title lowercase text-7xl md:text-9xl opacity-25 nordique-bold text-center'>Spark with us</h2>
+        <h2 className='home-spark-title lowercase text-7xl md:text-8xl opacity-25 nordique-bold text-center'>Spark with us</h2>
         <div>
             <HomeSparkMain />
         </div>
-        <p className='home-spark-title lowercase text-3xl md:text-5xl opacity-25 nordique-bold text-center'>Collaboration is the key to bringing brands{"'"} compaings to life.</p>
+        <p className='home-spark-title lowercase text-3xl md:text-5xl opacity-25 nordique-bold text-center w-4/5'>Collaboration is the key to bringing brands{"'"} compaings to life.</p>
     </div>
   )
 }
