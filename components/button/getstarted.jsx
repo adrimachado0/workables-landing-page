24c4803e1c0fdb38dc24d6f48d0bbf9dd192ef75
Adriel Machado <adri@newtonhewitt.com>
2023-07-03 13:23:11 -0300

    Cambios en la ui

--- components/button/getstarted.jsx
+++ components/button/getstarted.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const GetStarted = ({info}) => {
   return (
-    <p className='capitalize w-32 h-8 rounded-3xl text-white border-2 gradiente flex justify-center items-center border-none cursor-pointer'>{info}</p>
+    <p className='w-36 h-8 rounded-3xl text-white border-2 gradiente flex justify-center items-center border-none cursor-pointer'>{info}</p>
   )
 }
 
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React, { useEffect, useState } from 'react'
-import Logo from '../../assets/workablesLogo.png'
+import Logo from '../../assets/workablesLogo.webp'
+import LogoImagen from '../../assets/workablesLogo.png'
 import GetStarted from '../button/getstarted'
 
 import Hamburger from './img/hamburger.png';
@@ -28,13 +29,14 @@ const Header = ({setFixed}) => {
 
   return (
     <header className='absolute w-full flex gap-2 md:gap-5 justify-between items-center my-5 top-0'>
-      <div className='flex justify-around w-full md:mx-20 items-center'>
-        <Link className='z-10' href='/'><Image className='w-52' src={Logo} alt="Logo Workables" /></Link>
+      <div className='flex justify-center md:justify-around w-full md:mx-20 items-center'>
+        <Image width={30} height={30} onClick={() => setOpenMenu(true)} className='z-10 md:hidden cursor-pointer pt-2 mr-5' src={Hamburger} alt="Hamburger menu"/>
+        <Link className='z-10 md:hidden' href='/' ><Image className='w-44' src={Logo} alt="Logo Workables" /></Link>
+        <Link className='z-10 hidden md:block' href='/'><Image className='w-52' src={LogoImagen} alt="Logo Workables" /></Link>
         <div className='gap-2 md:gap-5 items-center hidden md:flex'>
             <Link href="/waitlist" className='cursor-pointer z-50'>Sign in</Link>
-            <GetStarted info="Get started" className='z-50 cursor-pointer' />
+            <Link href="/waitlist" className='cursor-pointer z-50'><GetStarted info="Join the waitlist" className='z-50 cursor-pointer' /></Link>
         </div>
-        <Image width={30} height={30} onClick={() => setOpenMenu(true)} className='z-10 md:hidden cursor-pointer pt-2' src={Hamburger} alt="Hamburger menu"/>
       </div>
         <div className={`z-50 absolute right-0 -top-5 ${openMenu ? 'w-full' : 'w-0'} transition-all duration-300 bg-white overflow-hidden`}>
               <div className={`md:hidden h-screen z-50 flex flex-col items-center`}>
